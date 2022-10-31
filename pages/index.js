40403936e25aab0e99ed1370dbf32af477a9f343
Marya <maryammuchai@gmail.com>
2022-10-31 21:49:32 +0300

    updating index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import nav from "../components/nav";
 
 export default function index() {
   return (
-    <div className="bg-gray-800 min-h-screen font-inter"></div>
+    <div className="bg-gray-800 min-h-screen font-inter">
 
-    
+      <nav></nav>    
+
+    </div>
+
   );
 }
